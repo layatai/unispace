b199e6ed6a49e21eb111ded8dab1c7c90596f560
layatai <layatai@gmail.com>
2026-08-14 22:54:23 +0700

    feat(application): add file transfer codec

---
+++ Sources/Application/FileTransferFrameCodec.swift
@@ -0,0 +1,32 @@
+import Foundation
+import UniSpaceDomain
+
+public enum FileTransferFrameCodec {
+    public static let maximumEncodedSize = FileTransferLimits.default.maximumChunkSize + 128 * 1_024
+
+    public static func encode(_ envelope: FileTransferEnvelope) throws -> Data {
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = .binary
+        let data = try encoder.encode(envelope)
+        guard data.count <= maximumEncodedSize else {
+            throw FileTransferProtocolError.invalidChunkSize(data.count)
+        }
+        return data
+    }
+
+    public static func decode(_ data: Data) throws -> FileTransferEnvelope {
+        guard !data.isEmpty, data.count <= maximumEncodedSize else {
+            throw FileTransferProtocolError.invalidChunkSize(data.count)
+        }
+        let envelope: FileTransferEnvelope
+        do {
+            envelope = try PropertyListDecoder().decode(FileTransferEnvelope.self, from: data)
+        } catch {
+            throw FileTransferProtocolError.malformedEnvelope
+        }
+        guard envelope.version == FileTransferEnvelope.protocolVersion else {
+            throw FileTransferProtocolError.unsupportedVersion(envelope.version)
+        }
+        return envelope
+    }
+}
